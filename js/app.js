c5f14352bf960d2e6a64f1c2f390e3a4473951b5
alfille <paul.alfille@gmail.com>
2026-09-03 20:33:53 -0400

    really stupid typo

--- js/app.js
+++ js/app.js
@@ -444,7 +444,7 @@ class DatabaseManager { // convenience class
 
     // reset authelia authorization
     checkAuth() {
-        if ( !navigator.online ) {
+        if ( !navigator.onLine ) {
 			console.log("navigator.onLine",navigator.onLine);
             return Promise.resolve({status: 'offline'}) ;
         }
