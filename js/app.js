f5de2df48edac572bda157aa2caa9a5050007177
Paul H Alfille <paul.alfille@gmail.com>
2026-09-09 10:42:08 -0400

    Work around GlobalPot

--- js/app.js
+++ js/app.js
@@ -313,7 +313,7 @@ export class CSV { // convenience class
     }
     
     make_table() {
-        globalPot.getAllIdDoc()
+        globalThis.globalPot.getAllIdDoc()
         .then( docs => docs.rows.map( r => this.make_row( this.columns.map( c => this.get_text( c, r.doc ) ) ) ) )
         .then( data => data.join("\n") )
         .then( data => [this.make_headings(), data].join("\n") )
